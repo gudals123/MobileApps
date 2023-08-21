a3568f51214d2b217018254d4c8d773818f3521e
gudals123 <104682516+gudals123@users.noreply.github.com>
2023-08-21 15:44:51 +0900

    feat: #5 임시저장

--- InstaProject/app/src/main/java/com/example/instaproject/navigation/HomeFragment.kt
+++ InstaProject/app/src/main/java/com/example/instaproject/navigation/HomeFragment.kt
@@ -5,12 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instaproject.R
-
+import com.example.instaproject.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.activity_main,container,false)
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        firestore = FirebaseFirestore.getInstance()
         return view
     }
+    inner class DetailViewrecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init {
+
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
 }
---
+++ InstaProject/app/src/main/java/com/example/instaproject/navigation/model/ContentDTO.kt
@@ -0,0 +1,15 @@
+package com.example.instaproject.navigation.model
+
+
+data class ContentDTO(var exception: String? = null,
+                       var imageUrl: String? = null,
+                       var uid:  String? = null,
+                       var userId:  String? = null,
+                       var timestamp: Long? = null,
+                       var favoriteCount: Int =0,
+                       var favorites: Map<String,Boolean> = HashMap()){
+    data class Comment(var uid: String? = null,
+                       var userId : String? = null,
+                       var comment: String? = null,
+                       var timestamp: Long? = null)
+}
