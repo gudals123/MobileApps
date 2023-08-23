288749a757c7dcbce72016181562dc06d675b8c0
namtaeu <113846111+namtaeu@users.noreply.github.com>
2023-08-23 19:58:13 +0900

    feat: #2 :로그인기능 및 디자인 일부 수정

--- InstaProject/app/src/main/java/com/example/instaproject/navigation/SearchFragment.kt
+++ InstaProject/app/src/main/java/com/example/instaproject/navigation/SearchFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.instaproject.R
+import com.example.instaproject.databinding.ActivityMyprofileBinding
+import com.example.instaproject.databinding.ActivitySearchBinding
 
 
 class SearchFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var binding = ActivitySearchBinding.inflate(layoutInflater)
         var view = LayoutInflater.from(activity).inflate(R.layout.activity_search,container,false)
         return view
     }
