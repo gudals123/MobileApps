efb694cdb97da3f37e2728a0d018f70760dcc180
gckbb <gckbb1201@gmail.com>
2023-08-29 18:40:12 +0900

    feat::#6 포스트업로드 기능 완료

--- InstaProject/app/src/main/java/com/example/instaproject/UploadExplainActivity.kt
+++ InstaProject/app/src/main/java/com/example/instaproject/UploadExplainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.instaproject
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -8,6 +9,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.instaproject.databinding.ActivityUploadExplainBinding
+import com.example.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
@@ -20,6 +24,8 @@ class UploadExplainActivity : AppCompatActivity() {
     var storage: FirebaseStorage? = null
     var photoUri: Uri? = null
     var bitmap: Bitmap? = null
+    var firestore : FirebaseFirestore? = null
+    var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,9 @@ class UploadExplainActivity : AppCompatActivity() {
         setContentView(binding.root)
         //Init Storage
         storage = FirebaseStorage.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
+        //이전 화면에서 받아온 사진 띄우기
         var imageType = intent.getIntExtra("typeNum", 1)
         if (imageType == 1) {
             photoUri = intent.getParcelableExtra<Uri>("path")
@@ -50,29 +58,13 @@ class UploadExplainActivity : AppCompatActivity() {
 
         //Done 버튼 클릭 시 사진, 글 내용, 태그 업로드
         binding.btnUploadDone.setOnClickListener {
-            //Make filename
-            var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-            var imageFileName = "IMAGE_" + timestamp + "_.png"
-            var storageRef = storage?.reference?.child("images")?.child(imageFileName)
-
+            //사진 업로드 기능
             if (imageType == 1) {
-                //FileUpload
-                storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-                    Toast.makeText(this, "업로드 완료", Toast.LENGTH_LONG).show()
-                }
-
+                //갤러리 선택 사진 업로드
+                contentUploadLib()
             } else if (imageType == 2) {
-                //FileUpload
-                val bitmapFile = bitmap
-                val baos = ByteArrayOutputStream()
-                bitmapFile!!.compress(Bitmap.CompressFormat.PNG, 100, baos)
-                val data = baos.toByteArray()
-                storageRef?.putBytes(data!!)?.addOnSuccessListener {
-                    Toast.makeText(this, "업로드 완료", Toast.LENGTH_LONG).show()
-                }
-
-                //storageRef?.putBytes(bitmap!!)?.addOnSuccessListener {
-                //    Toast.makeText(this, "업로드 완료", Toast.LENGTH_LONG).show()
+                //카메라 선택 사진 업로드
+                contentUploadCam()
             }
 
             val intent = Intent(this, MainActivity::class.java)
@@ -81,16 +73,75 @@ class UploadExplainActivity : AppCompatActivity() {
 
     }
 
-    fun contentUpload() {
+    fun contentUploadLib() {
         //Make filename
         var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         var imageFileName = "IMAGE_" + timestamp + "_.png"
-
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
         //FileUpload
         storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this, "업로드 완료", Toast.LENGTH_LONG).show()
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var ContentDTO = ContentDTO()
+
+                //Insert downloadUrl of image
+                ContentDTO.imageUrl = uri.toString()
+                //Insert uid of user
+                ContentDTO.uid = auth?.currentUser?.uid
+                //Insert userId
+                ContentDTO.userId = auth?.currentUser?.email
+                //Insert explain
+                ContentDTO.explain = binding.editEdittext.text.toString()
+                //Insert tag
+                ContentDTO.tag = binding.insertTagEdittext.text.toString()
+                //Insert timestamp
+                ContentDTO.timestamp = System.currentTimeMillis()
+
+                firestore?.collection("images")?.document()?.set(ContentDTO)
+
+                setResult(Activity.RESULT_OK)
+                Toast.makeText(this, "업로드 완료", Toast.LENGTH_SHORT).show()
+
+                finish()
+            }
+        }
+    }
+
+    fun contentUploadCam() {
+        //Make filename
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        //카메라 선택 사진 업로드
+        val bitmapFile = bitmap
+        val baos = ByteArrayOutputStream()
+        bitmapFile!!.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        val data = baos.toByteArray()
+        storageRef?.putBytes(data!!)?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var ContentDTO = ContentDTO()
+
+                //Insert downloadUrl of image
+                ContentDTO.imageUrl = uri.toString()
+                //Insert uid of user
+                ContentDTO.uid = auth?.currentUser?.uid
+                //Insert userId
+                ContentDTO.userId = auth?.currentUser?.email
+                //Insert explain
+                ContentDTO.explain = binding.editEdittext.text.toString()
+                //Insert tag
+                ContentDTO.tag = binding.insertTagEdittext.text.toString()
+                //Insert timestamp
+                ContentDTO.timestamp = System.currentTimeMillis()
+
+                firestore?.collection("images")?.document()?.set(ContentDTO)
+
+                setResult(Activity.RESULT_OK)
+                Toast.makeText(this, "업로드 완료", Toast.LENGTH_SHORT).show()
+
+                finish()
+            }
         }
     }
 }
